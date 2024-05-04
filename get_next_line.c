dbaa8b8f3d6e007e120ec1a5b9ffe21c041d5f44
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-05-04 08:36:22 +0000

    message

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdavurov <rdavurov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 12:50:10 by rdavurov          #+#    #+#             */
+/*   Updated: 2024/05/02 12:50:11 by rdavurov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+    
+}
+
+// BS = 5
+
+// feafqefqwee
+// qfqefqwefqwf
+// qefqefqf
+
+// buffer = 
+// static char *string = feafqefqwee
+// qfq
+// processString(string)
+// {
+//     string = qfq
+//     return (string - \nqfq)
+// }
