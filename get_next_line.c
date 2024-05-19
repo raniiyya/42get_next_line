12a967e900270f8832085178222eeab29aba076f
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-05-19 12:41:13 +0000

    update

--- get_next_line.c
+++ get_next_line.c
@@ -12,22 +12,63 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+static char *read_file(int fd, char *store, char *buffer)
 {
-    
+	char		*tmp;
+	ssize_t		bytes_read;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (NULL);
+		if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		if (!store)
+			store = ft_strdup("");
+		tmp = store;
+		store = ft_strjoin(tmp, buffer);
+		free(tmp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (store);
 }
 
-// BS = 5
+static char	*process_store(char *line)
+{
+	int		i;
+	char	*rest;
 
-// feafqefqwee
-// qfqefqwefqwf
-// qefqefqf
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == 0 || line[i + 1] == 0)
+		return (NULL);
+	rest = ft_substr(line, i + 1, ft_strlen(line + i + 1));
+	if (!rest)
+		return (NULL);
+	line[i + 1] = '\0';
+	return (rest);
+}
 
-// buffer = 
-// static char *string = feafqefqwee
-// qfq
-// processString(string)
-// {
-//     string = qfq
-//     return (string - \nqfq)
-// }
+char	*get_next_line(int fd)
+{
+	static char	*store = NULL;
+	char		*line;
+	char		*buffer;
+
+	if (fd < 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	line = read_file(fd, store, buffer);
+	free(buffer);
+	if (!line)
+		return (NULL);
+	store = process_store(line);
+	return (line);
+}
